1659e9b0ea9837d3e0bfa8102b3b13325ad065ef
AnikyMX <71912870+AnikyMX@users.noreply.github.com>
2026-01-03 10:35:38 +0800

    Optimize asset management and remove error logs
    Removed unused error logging and optimized pointer checks. Updated asset management to drop old data without retaining it.

--- src/aasset.rs
+++ src/aasset.rs
@@ -9,17 +9,16 @@ use ndk_sys::{AAsset, AAssetManager};
 use std::{
     cell::UnsafeCell,
     collections::HashMap,
-    error::Error,
+    // error::Error, // Tidak dipake lagi karena log dimatiin
     ffi::{CStr, OsStr},
     io::{self, Read, Seek},
     os::unix::ffi::OsStrExt,
     path::Path,
-    //    ptr,
     sync::{LazyLock, Mutex},
 };
+
 static MC_FILELOADER: LazyLock<Mutex<FileLoader>> = LazyLock::new(|| Mutex::new(FileLoader::new()));
-// This makes me feel wrong... but all we will do is compare the pointer
-// and the struct will be used in a mutex so this is safe??
+
 #[derive(PartialEq, Eq, Hash)]
 struct AAssetPtr(*const ndk_sys::AAsset);
 unsafe impl Send for AAssetPtr {}
@@ -33,32 +32,32 @@ pub unsafe extern "C" fn open(
     fname: *const c_char,
     mode: c_int,
 ) -> *mut AAsset {
-    // This is where UB can happen, but we are merely a hook.
     let aasset = unsafe { ndk_sys::AAssetManager_open(man, fname, mode) };
-    let pointer = match std::ptr::NonNull::new(man) {
-        Some(yay) => yay,
-        None => {
-            log::warn!("AssetManager is null?, preposterous, mc detection failed");
-            return aasset;
-        }
+    
+    // [OPTIMISASI] Langsung cek pointer tanpa Log Warning
+    let Some(pointer) = std::ptr::NonNull::new(man) else {
+        return aasset;
     };
+
     let manager = unsafe { ndk::asset::AssetManager::from_ptr(pointer) };
     let c_str = unsafe { CStr::from_ptr(fname) };
     let raw_cstr = c_str.to_bytes();
     let os_str = OsStr::from_bytes(raw_cstr);
     let c_path: &Path = Path::new(os_str);
+    
     let mut sus = MC_FILELOADER.lock().ignore_poison();
     if let Some(yay) = sus.get_file(c_path, manager) {
         WANTED_ASSETS.get_mut().insert(AAssetPtr(aasset), yay);
     }
     aasset
 }
+
 macro_rules! handle_result {
     ($expr:expr) => {
         match $expr {
             Ok(val) => val,
-            Err(e) => {
-                log::error!("{e}");
+            Err(_) => {
+                // [OPTIMISASI] Log Error dimatikan (Silent Fail)
                 return -1;
             }
         }
@@ -88,7 +87,7 @@ pub unsafe extern "C" fn read(aasset: *mut AAsset, buf: *mut c_void, count: size
         Some(file) => file,
         None => return ndk_sys::AAsset_read(aasset, buf, count),
     };
-    // Reuse buffer given by caller
+    
     let rs_buffer = core::slice::from_raw_parts_mut(buf as *mut u8, count);
     let read_total = handle_result!((*file).read(rs_buffer));
     handle_result!(read_total.try_into())
@@ -132,9 +131,17 @@ pub unsafe extern "C" fn rem64(aasset: *mut AAsset) -> off64_t {
 
 pub unsafe extern "C" fn close(aasset: *mut AAsset) {
     let wanted_assets = WANTED_ASSETS.get_mut();
-    if let Some(buffer) = wanted_assets.remove(&AAssetPtr(aasset)) {
-        MC_FILELOADER.lock().ignore_poison().last_buffer = Some(buffer);
-    }
+    
+    // --- [MODIFIKASI KRUSIAL] ---
+    // Hapus data dari WANTED_ASSETS tanpa menyimpannya kembali ke last_buffer.
+    // Biarkan data lama musnah (dropped) agar memori bersih.
+    wanted_assets.remove(&AAssetPtr(aasset)); 
+    
+    // Kode lama yang dihapus:
+    // if let Some(buffer) = wanted_assets.remove(&AAssetPtr(aasset)) {
+    //     MC_FILELOADER.lock().ignore_poison().last_buffer = Some(buffer);
+    // }
+
     ndk_sys::AAsset_close(aasset);
 }
 
@@ -144,7 +151,6 @@ pub unsafe extern "C" fn get_buffer(aasset: *mut AAsset) -> *const c_void {
         Some(file) => file,
         None => return ndk_sys::AAsset_getBuffer(aasset),
     };
-    // Let's hope this does not go boom boom
     file.get_ref().as_ptr().cast()
 }
 
@@ -156,7 +162,7 @@ pub unsafe extern "C" fn fd_dummy(
     let wanted_assets = WANTED_ASSETS.get_mut();
     match wanted_assets.get(&AAssetPtr(aasset)) {
         Some(_) => {
-            log::error!("WE GOT BUSTED NOOO");
+            // [OPTIMISASI] Log Error dimatikan
             -1
         }
         None => ndk_sys::AAsset_openFileDescriptor(aasset, out_start, out_len),
@@ -171,7 +177,7 @@ pub unsafe extern "C" fn fd_dummy64(
     let wanted_assets = WANTED_ASSETS.get_mut();
     match wanted_assets.get(&AAssetPtr(aasset)) {
         Some(_) => {
-            log::error!("WE GOT BUSTED NOOO");
+            // [OPTIMISASI] Log Error dimatikan
             -1
         }
         None => ndk_sys::AAsset_openFileDescriptor64(aasset, out_start, out_len),
@@ -189,21 +195,20 @@ pub unsafe extern "C" fn is_alloc(aasset: *mut AAsset) -> c_int {
 fn seek_facade(offset: i64, whence: c_int, file: &mut Buffer) -> i64 {
     let offset = match whence {
         libc::SEEK_SET => {
-            //Let's check this so we don't mess up
             let u64_off = handle_result!(u64::try_from(offset));
             io::SeekFrom::Start(u64_off)
         }
         libc::SEEK_CUR => io::SeekFrom::Current(offset),
         libc::SEEK_END => io::SeekFrom::End(offset),
         _ => {
-            log::error!("Invalid seek whence");
+            // Log Error dimatikan
             return -1;
         }
     };
     match file.seek(offset) {
         Ok(new_offset) => handle_result!(new_offset.try_into()),
-        Err(err) => {
-            log::error!("seek Error: {err}");
+        Err(_) => {
+            // Log Error dimatikan
             return -1;
         }
     }
